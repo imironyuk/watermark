7193a39ef3762bb3bfbf92a5ed0262171a04a08c
Danchik <imironyuk1979@gmail.com>
2020-10-13 17:51:01 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,13 +7,13 @@ PROG_BTN = "_pib_"
 progflash_layout = [[sg.T(" "* 5),sg.T("Flash Programmig for Microocntroller"),sg.T(" "* 5)],
                             [sg.T(" "* 6),sg.Frame(layout= [[sg.T("Select file A"),sg.T(" "* 5),
                                                 sg.FileBrowse (key=ISO_HEX_FILE,target="-hexfilein-",
-                                                                file_types=(("HEX Files", "*.hex"),)),sg.T(" "* 5)]], title= "A",
+                                                                file_types=(('PNG pictures', '*.png'), ('JPEG pictures', '*.jpg'),)),sg.T(" "* 5)]], title= "A",
                                                                 title_location="n")],
                             [(sg.T(" "* 7))],
                             [sg.T(" "* 6),sg.Frame ( layout=[[sg.T ( "Select file for B" ),sg.T(" "* 20),
                                                  sg.FileBrowse ( key=PROBE_HEX_FILE,target="-hexfilein-",
                                                                  initial_folder=r"P:\CTM 100101-100200\100197 TankUS 1.25in ATEX probe\Firmware\Released",
-                                                                 file_types=(("HEX Files", "*.hex"),) ),
+                                                                 file_types=(('PNG pictures', '*.png'), ('JPEG pictures', '*.jpg'),)),
                                                  sg.T ( " " * 5 )]], title="B",title_location="n" )],
                             [sg.Text('File'), sg.Input(key="-hexfilein-",size=(100,1))],
                             [sg.T(" "*4), sg.Button("Начать",button_color=('white', '#28a745') , size=(20,3),key = PROG_BTN,disabled=False)],
@@ -25,7 +25,8 @@ progflash_layout = [[sg.T(" "* 5),sg.T("Flash Programmig for Microocntroller"),s
                                         title_location='n' )]]
 
 progFlashWin = sg.Window ( "Program Flash", size=(800,550)).Layout ( progflash_layout )
-flashEvent, flashValue = progFlashWin.Read ( timeout=100 )
+flashEvent, flashValue = progFlashWin.Read ( timeout=1 )
+print("^C")
 
 while True:
-    flashEvent, flashValue = progFlashWin.Read ( timeout=100 )
+    flashEvent, flashValue = progFlashWin.Read ( timeout=1 )
